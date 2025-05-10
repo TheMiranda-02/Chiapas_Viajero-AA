5b28eed336a10dfb631e800ac8b163e3ec29952c
TheMiranda02 <jose.guzman26@unach.mx>
2025-05-10 10:40:06 -0600

    Agregar archivos de proyecto.

---
+++ Pagina_Principal/IniciarSesion.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pagina_Principal
+{
+    /// <summary>
+    /// Lógica de interacción para IniciarSesion.xaml
+    /// </summary>
+    public partial class IniciarSesion : Window
+    {
+        public IniciarSesion()
+        {
+            InitializeComponent();
+        }
+
+
+        //PARA EL TXTEMAIL DE EMAIL
+        private void txtEmail_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Si el texto es "email", lo borra y cambia el color del texto a negro
+            if (txtEmail.Text == "email")
+            {
+                txtEmail.Text = "";
+                txtEmail.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txtEmail_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Si el campo está vacío, muestra el texto "email"
+            if (string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                txtEmail.Text = "email";
+                txtEmail.Foreground = new SolidColorBrush(Color.FromRgb(118, 113, 113));
+            }
+        }
+
+
+        //PARA EL TXTCONTRASEÑA DE CONTRASEÑA
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            passwordPlaceholder.Visibility = string.IsNullOrEmpty(txtPassword.Password)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        private void txtPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassword.Password))
+                passwordPlaceholder.Visibility = Visibility.Collapsed;
+        }
+
+        private void txtPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassword.Password))
+                passwordPlaceholder.Visibility = Visibility.Visible;
+        }
+
+
+    }
+}
---
+++ Pagina_Principal/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pagina_Principal
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIniciarSesion(object sender, RoutedEventArgs e)
+        {
+            IniciarSesion ventana = new IniciarSesion();
+            ventana.Show();
+
+            this.Close();
+        }
+
+
+    }
+}
